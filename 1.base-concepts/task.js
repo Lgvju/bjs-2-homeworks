1a4a3af6d8f446cf96b9a7cee76c0af82b0e64d1
OEM <gurleva_ludmila@mail.ru>
2023-11-17 02:15:58 +0300

    Задача с квадратным уравнением

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+    let discriminant = [];
+    discriminant = ((Math.pow(b, 2)) - (4 * a * c));
+    if (discriminant > 0) {
+        let solve = [];
+        solve[0] = ((-b + Math.sqrt(discriminant)) / (2 * a));
+        solve[1] = ((-b - Math.sqrt(discriminant)) / (2 * a));
+        discriminant = solve;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    } else if (discriminant === 0) {
+               let solve = [];
+               solve[0] = (-b / (2 * a));
+               discriminant = solve;
+    } else {
+        discriminant = [];
+    }
+    return discriminant;
 }
+    //let result = solveEquation(1, -4, 3);
+    //console.log(result);
+
