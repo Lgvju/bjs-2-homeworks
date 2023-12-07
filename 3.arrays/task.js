21f665e2ee4c34fd98e14a46271a33e782bb1297
OEM <gurleva_ludmila@mail.ru>
2023-12-08 00:28:59 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index])) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(users => users.gender === gender).map(users => users.age).reduce((age, item, index, users) => {
+        age += item;
+        if (index === users.length - 1) {
+            return age / users.length;
+        }
+        return age
+    }, 0);
+    return result;
 }
