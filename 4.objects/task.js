1fb962cf95788faa2688f82230ef50c76e8ef5b6
OEM <gurleva_ludmila@mail.ru>
2023-12-12 23:43:02 +0300

    Четвертое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined) {
+        let arr = this.marks;
+        this.marks = [...arr, ...marks];
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined) {
+        return 0;
+    } else if (this.marks.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (i = 0; i < this.marks.length; i++) {
+            sum = sum + this.marks[i];
+        }
+        return sum / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
+  
+
