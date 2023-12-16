881cded6ee95e7ca5d9356e74f8e46179404b45e
OEM <gurleva_ludmila@mail.ru>
2023-12-16 23:39:43 +0300

    Задание  готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿ function parseCount(products) {
+    if (Number.isNaN(Number.parseFloat(products))) {
+        throw new Error("Невалидное значение");
+    }
+    return parseFloat(products);
+}
+
+function validateCount(products) {
+    try {
+        return parseCount(products);
+    } catch (error) {
+        return error;
+    }
+}
+    // Задача 2
+
+class Triangle {
+    constructor(sideOne, sideTwo, sideThree) {
+        this.sideOne = sideOne;
+        this.sideTwo = sideTwo;
+        this.sideThree = sideThree;
+        if (
+             (sideOne + sideTwo) < sideThree ||
+             (sideOne + sideThree) < sideTwo ||
+             (sideTwo + sideThree) < sideOne
+        ) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        return +(this.sideOne + this.sideTwo + this.sideThree);
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        return +( Math.sqrt(p * (p - this.sideOne) * (p - this.sideTwo) * (p - this.sideThree)) ).toFixed(3);
+
+    }
+}
+
+function getTriangle(sideOne, sideTwo, sideThree) {
+  try {
+    return new Triangle(sideOne, sideTwo, sideThree);
+  } catch(error) {
+      return {
+        get perimeter() {
+          return `Ошибка! Треугольник не существует`;
+        },
+        get area() {
+          return `Ошибка! Треугольник не существует`;
+        }
+      }
+  }
+}
