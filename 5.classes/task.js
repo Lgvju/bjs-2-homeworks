6bc87bf51c83186961b43878d590e55316ce8184
OEM <gurleva_ludmila@mail.ru>
2023-12-16 16:50:06 +0300

    Пятое задание дополнительное

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,6 +62,8 @@ class DetectiveBook extends Book {
     }
 }
 
+// Задача 2
+
 class Library {
     constructor(name, books) {
         this.name = name;
@@ -69,27 +71,28 @@ class Library {
     }
 
     addBook(book) {
-        if (book._state > 30) {
+        if (book.state > 30) {
             this.books.push(book);
         }
     }
-
-    findBookBy(type, value) {
-        for (let book of this.books) {
-            if (
-                (type === "author" && book.author === value) ||
-                (type === "name" && book.name === value) ||
-                (type === "releaseDate" && book.releaseDate === value) ||
-                (type === "pagesCount" && book.pagesCount === value) ||
-                (type === "state" && book.state === value) ||
-                (type === "type" && book.type === value)
-            ) {
-                return book;
-            }
-        }
-        return null;
-    }
-
+    /*findBookBy(type, value) {  //Первый вариант поиска findBookBy
+         for (let book of this.books) {
+             if (
+                 (type === "author" && book.author === value) ||
+                 (type === "name" && book.name === value) ||
+                 (type === "releaseDate" && book.releaseDate === value) ||
+                 (type === "pagesCount" && book.pagesCount === value) ||
+                 (type === "state" && book.state === value) ||
+                 (type === "type" && book.type === value)
+             ) {
+                 return book;
+             }
+         }
+         return null;
+     }*/
+    findBookBy(type, value) { //Второй вариант поиска findBookBy
+        return this.books.find(book => book[type] === value) || null;
+  }
     giveBookByName(bookName) {
         for (let i = 0; i < this.books.length; i++) {
             if (this.books[i].name === bookName) {
@@ -101,3 +104,51 @@ class Library {
         return null;
     }
 }
+
+// Задача 3
+
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
+  addMark(mark, subject) {
+      if (this.marks?.[subject] === undefined && mark >= 2 && mark <= 5) {
+      this.marks[subject] = [];
+      this.marks[subject].push(mark);
+    } else if (this.marks.hasOwnProperty(subject) && mark >= 2 && mark <= 5) {
+      this.marks[subject].push(mark);
+    }
+  }
+  getAverageBySubject(subject) {
+    if (this.marks.hasOwnProperty(subject) === false) {
+      return 0;
+    }
+    const gradePointAverage = this.marks[subject].reduce((acc, mark, index, arr) => {
+      acc += mark;
+      if (index === arr.length - 1) {
+        return acc / arr.length;
+      }
+      return acc;
+    }, 0);
+    return gradePointAverage;
+  }
+  getAverage() {
+    if (Object.keys(this.marks).length === 0) {
+      return 0;
+    }
+    let allSubject = Object.keys(this.marks);
+    for (let subject of allSubject) {
+      allSubject[allSubject.indexOf(subject)] = this.getAverageBySubject(subject);
+    }
+    const gradePointAverage = allSubject.reduce((acc, mark, index, arr) => {
+      acc += mark;
+      if (index === arr.length - 1) {
+          return acc / arr.length;
+      }
+      return acc;
+      }, 0);
+      return gradePointAverage;
+  }
+}
+
